45ee7756aad9498939a60a94ffe8795b32da9dbd
bimboIlya <moonl1ght24rus@gmail.com>
2021-02-20 01:13:27 +0700

    добавил немного функциональной магии в обертку Result

--- app/src/main/java/com/example/cft_final_project/common/network/Result.kt
+++ app/src/main/java/com/example/cft_final_project/common/network/Result.kt
@@ -1,6 +1,17 @@
 package com.example.cft_final_project.common.network
 
 sealed class Result<out R> {
+
+    fun handle(
+        onSuccess: (R) -> Unit = { },
+        onFailure: (ex: Throwable) -> Unit = { }
+    ) {
+        when (this) {
+            is Success -> onSuccess(this.data)
+            is Error -> onFailure(this.exception)
+        }
+    }
+
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Throwable) : Result<Nothing>()
 }
